e0d8892d4fad489bc1edc49b20e2c785c1e1e4ce
Mozi-k <moses@everlytic.com>
2018-10-04 15:02:12 +0200

    Add geolocation support via geolaction api

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 const expect = require('expect');
 
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
   it('should generate the correct message object', () => {
@@ -13,5 +13,20 @@ describe('generateMessage', () => {
     // expect(message.text).toBe(text);
     expect(message).toInclude({from, text});
     expect(message.createdAt).toBeA('number');
+  });
+});
+
+describe('generateLocationMessage', () => {
+  it('should generate correct location object', () => {
+    var from = 'Username';
+    var latitude = 27;
+    var longitude = 80;
+    var url = 'https://google.com/maps?q=27,80';
+
+    var locationMessage = generateLocationMessage(from, latitude, longitude);
+
+    expect(locationMessage).toInclude({from, url});
+    expect(locationMessage.url).toBe(url);
+    expect(locationMessage.createdAt).toBeA('number');
   });
 });
